ff395488f113416c61cb8b93dc96247deac3eae5
Andrew Davidson <andrew@amdavidson.com>
2020-03-11 08:38:15 -0400

    adding gist backup

--- github-backup.py
+++ github-backup.py
@@ -8,18 +8,18 @@ import pygit2
 from lib import Log
 
 user = "amdavidson"
-backupdir = Path.home() / "backups/github/"
+backupdir = Path.home() / "backups/github"
 log = Log()
 log.LOGLEVEL = 1
 
-def backup_github(): 
+def backup_gh_repos():
     response = requests.get("https://api.github.com/users/"+user+"/repos")
 
     repos = json.loads(response.text)
 
     for repo in repos:
         log("Backing up: " + repo["name"])
-        dest = backupdir / repo["name"]
+        dest = backupdir / "repo" / repo["full_name"]
         localrepopath = pygit2.discover_repository(str(dest))
         if localrepopath == None:
             log("Cloning repo...")
@@ -28,5 +28,24 @@ def backup_github():
             log("Fetching updates...")
             localrepo = pygit2.Repository(localrepopath).remotes["origin"].fetch()
 
+def backup_gh_gists():
+    response = requests.get("https://api.github.com/users/"+user+"/gists")
+
+    gists = json.loads(response.text)
+
+    for gist in gists:
+        log("Backing up: " + gist["id"])
+        dest = backupdir / "gist" / gist["owner"]["login"] / gist["id"]
+        localgistpath = pygit2.discover_repository(str(dest))
+        if localgistpath == None:
+            log("Cloning gist...")
+            pygit2.clone_repository(gist['git_pull_url'], str(dest), bare=True)
+        else:
+            log("Fetching updates...")
+            pygit2.Repository(localgistpath).remotes["origin"].fetch()
+
+def backup_github(): 
+    backup_gh_repos()
+    backup_gh_gists()
 
 backup_github()
