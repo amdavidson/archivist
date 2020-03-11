e2467a54c3a7714cce07da8b8954708438e782cb
Andrew Davidson <andrew@amdavidson.com>
2020-03-11 15:43:54 -0400

    placeholder commit

---
+++ fastmail.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import imaplib
+
+from lib import Log
+
+log = Log()
+log.LOGLEVEL = 1
+
+
+def connect_server():
+    
+
+def backup_email():
+
+
+
+
+def backup_fastmail():
+    connect_server()
+    backup_email()
+
+
+backup_fastmail()
+
