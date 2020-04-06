4a9b677dad063eecfd6d6a66b8ffc53637ebf13f
Andrew Davidson <andrew@amdavidson.com>
2020-04-06 09:21:48 -0400

    changing package structure and adding log level control option, fixes #8

--- archivist/__init__.py
+++ archivist/__init__.py
@@ -1,10 +1,16 @@
 import logging, sys
 
 log = logging.getLogger(__name__)
-log.setLevel(logging.INFO)
-out_handler = logging.StreamHandler(sys.stdout)
-#out_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
-out_handler.setLevel(logging.INFO)
-log.addHandler(out_handler)
 
-from archivist.archivist import Archivist
+def setup_logging(config):
+    loglevel = config.get("loglevel", "info")
+    if loglevel == "info":
+        log.setLevel(logging.INFO)
+    elif loglevel == "warn":
+        log.setLevel(logging.WARNING)
+    elif loglevel == "error":
+        log.setLevel(logging.ERROR)
+    out_handler = logging.StreamHandler(sys.stdout)
+    #out_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+    out_handler.setLevel(logging.INFO)
+    log.addHandler(out_handler)
