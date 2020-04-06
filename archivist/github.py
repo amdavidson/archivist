d466a6ba0ddf8b1679bd73c9e6aa4eb645c6c256
Andrew Davidson <andrew@amdavidson.com>
2020-04-06 16:40:04 -0400

    Added support for config specifying specific repos and gists to backup, avoiding need to backup entire github account.
    repo_backup_list:
    - Repo_name1
    - Repo_name2
    
    gist_backup_list:
    - gist_id1
    - gits_id2

--- archivist/github.py
+++ archivist/github.py
@@ -4,12 +4,20 @@ import pygit2
 
 log = logging.getLogger(__name__)
 
-def backup_gh_repos(user, backupdir):
+def backup_gh_repos(user, backupdir, backup_list=None):
     response = requests.get("https://api.github.com/users/"+user+"/repos")
-
     repos = json.loads(response.text)
 
-    for repo in repos:
+    to_backup = []
+
+    if backup_list != None:
+        for repo in repos:
+            if repo["name"] in backup_list:
+                to_backup.append(repo)
+    else:
+        to_backup = repos
+
+    for repo in to_backup:
         log.info("Backing up: " + repo["name"])
         dest = backupdir / "repo" / repo["full_name"]
         localrepopath = pygit2.discover_repository(str(dest))
@@ -20,12 +28,19 @@ def backup_gh_repos(user, backupdir):
             log.info("Fetching updates...")
             localrepo = pygit2.Repository(localrepopath).remotes["origin"].fetch()
 
-def backup_gh_gists(user, backupdir):
+def backup_gh_gists(user, backupdir, backup_list=None):
     response = requests.get("https://api.github.com/users/"+user+"/gists")
-
     gists = json.loads(response.text)
 
-    for gist in gists:
+    to_backup = []
+    if backup_list != None:
+        for gist in gists:
+            if gist["id"] in backup_list:
+                to_backup.append(gist)
+    else:
+        to_backup = gists
+
+    for gist in to_backup:
         log.info("Backing up: " + gist["id"])
         dest = backupdir / "gist" / gist["owner"]["login"] / gist["id"]
         localgistpath = pygit2.discover_repository(str(dest))
@@ -40,5 +55,5 @@ def backup_github(config):
     disable_gists = config.get("disable_gists", False)
     disable_repos = config.get("disable_repos", False)
     github_dir = Path(config["backup_folder"])
-    if not disable_repos: backup_gh_repos(config["user"], github_dir)
-    if not disable_gists: backup_gh_gists(config["user"], github_dir)
+    if not disable_repos: backup_gh_repos(config["user"], github_dir, config.get("repo_backup_list", None))
+    if not disable_gists: backup_gh_gists(config["user"], github_dir, config.get("gist_backup_list", None))
