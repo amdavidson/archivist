611e757dee38d3adbcc1cd24b015690399498adc
Andrew Davidson <andrew@amdavidson.com>
2020-07-07 12:37:12 -0400

    fixing error in cron script that expected tty

--- bin/cron.sh
+++ bin/cron.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$CURRENTDIR/.."
 
 cd $ROOT_DIR
 
-docker run --rm -it \
+docker run --rm  \
     -v $(pwd)/archivist.yml:/archivist/archivist.yml \
     -v /tmp/backups:/backups \
     amdavidson/archivist:latest $@
